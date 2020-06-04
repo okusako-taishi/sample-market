7b65e5bb697dce4c91e0b09b2049a4b273b17af5
nishimoto-keita <aggrest0306@gmail.com>
2020-06-04 19:05:59 +0900

    erbからhaml変換

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :users, only: show
+
+  resources :users, only: [:show]
   resources :items, only: [:show, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
