c5f55cfca8e06cc7cb9005904c6261c9238d046e
hizikataraiden12 <hizikataraiden12@gmail.com>
2020-06-08 18:06:30 +0900

    商品購入確認V1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'items#index'
+  root 'items#buy'
   resources :users, only: :show
-  resources :items, only: [:show, :new]
+  resources :items, only: [:show, :new] do
+    member do
+      get 'buy'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :items, only: [:new]
 end
