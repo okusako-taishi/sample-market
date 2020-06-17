edcf2f4c97b2454ede1454333103f26b0e457d95
gain009 <63448529+gain009@users.noreply.github.com>
2020-06-17 19:45:04 +0900

    とりあえず、一回なかったことに……
    もう知らん(諦め)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,10 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy, :buy, :pay]
+  before_action :item_params, only: :create
   before_action :set_item, except: [:index, :new, :create,:get_category_children,:get_category_grandchildren]
 
   def index
-    @items = Item.includes(:images)
+    @items = Item.all
   end
 
   def new
@@ -36,7 +38,8 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.save
+    if @item.valid?
+      @item.save
       redirect_to root_path
     else
       #セレクトボックスの初期値設定
@@ -47,6 +50,26 @@ class ItemsController < ApplicationController
     end
       render :new
     end
+  end
+
+  def buy
+    if card.exists?
+      @card     = Card.find_by(user_id: current_user.id)
+      Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_PRIVATE_KEY)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = Payjp::Customer.retrieve(@card.customer_id).cards.data[0]
+    end
+  end
+
+  def pay
+    @card = Card.find_by(user_id: current_user.id)
+    @item.save!
+    Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_PRIVATE_KEY)
+    @charge = Payjp::Charge.create(
+    amount: @product.price,
+    customer: @card.customer_id,
+    currency: 'jpy'
+    )
   end
 
   def edit
@@ -67,14 +90,12 @@ class ItemsController < ApplicationController
   end
 
   private
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :status, :cost, :prefecture_id, :days, :price, images_attributes: [:url, :_destroy, :id], brand_attributes: [:name],).merge(user_id: current_user.id).merge(saler_id: current_user.id)
   end
 
-  def set_item
-    @item = Item.find(params[:id])
-    @items = Item.all
-    @items = Item.includes(:images, :brands)
-  end
 end
