cabacc6c578d06eaa7332d8686993029b8f932b4
okusako-taishi <aopotaishi@gmail.com>
2020-06-18 12:28:33 +0900

    商品編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -54,20 +54,11 @@ class ItemsController < ApplicationController
       grandchild_category = @item.category
       child_category = grandchild_category.parent
 
-      @category_parent_array = []
-      Category.where(ancestry: nil).each do |parent|
-        @category_parent_array << parent.name
-      end
+      @category_parent_array = Category.where(ancestry: nil).pluck(:name)
 
-      @category_children_array = []
-      Category.where(ancestry: child_category.ancestry).each do |children|
-        @category_children_array << children
-      end
+      @category_children_array = Category.where(ancestry: child_category.ancestry).pluck(:name)
 
-      @category_grandchildren_array = []
-      Category.where(ancestry: grandchild_category.ancestry).each do |grandchildren|
-        @category_grandchildren_array << grandchildren
-      end
+      @category_grandchildren_array = Category.where(ancestry: grandchild_category.ancestry).pluck(:name)
     else 
       render :index
     end
