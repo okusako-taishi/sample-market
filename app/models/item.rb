4cc7258f4965e57232db19297cf3a6000610a31f
nishimoto-keita <aggrest0306@gmail.com>
2020-06-18 10:58:08 +0900

    モデルとマイグレーション作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,12 @@
 class Item < ApplicationRecord
+  has_many :comments, dependent: :destroy
+
+  belongs_to :seller, class_name: "User" , optional: true,foreign_key: "seller_id"
+  belongs_to :buyer, class_name: "User", optional: true,foreign_key: "buyer_id"
 
   belongs_to :user
   belongs_to :saler, class_name: "User", optional: true
-  belongs_to :buyer, class_name: "User", optional: true
+  belongs_to :buyer, class_name: "User", optional: true, foreign_key: "auction_id"
   
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
